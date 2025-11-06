006f505d0679a5ff0af7fee311134a7f4ecaad25
Jan Starý <hans@stare.cz>
2025-11-06 18:50:43 +0100

    harmonic sries: only print with -v

--- harmf.c
+++ harmf.c
@@ -5,23 +5,54 @@
  * to the relatively large sum no longer makes a difference. For 32bit floats,
  * adding 1/2097152 (0x35000000) no longer increases 0x4176757c */
 
+/* With -v, print every step;
+ * most of the time is spent in the printing. */
+
 #include <stdint.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <math.h>
+#include <err.h>
 
 #define FMT "%7u  %15.13f %#010x  %21.18f %#010x\n"
 
-int
-main(void)
+int vflag = 0;
+
+void
+usage(void)
 {
+	errx(1, "usage: harm [-v]");
+}
+
+int
+main(int argc, char** argv)
+{
+	int c;
 	uint32_t n = 1;
 	float x = 1/n;
 	float this = 0;
 	float next = x;
 
+	while ((c = getopt(argc, argv, "v")) != -1) switch(c) {
+		case 'v':
+			vflag = 1;
+			break;
+		default:
+			usage();
+			break;
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc)
+		usage();
+
 	do {
 		this = next;
-		printf(FMT, n, x, *(uint32_t*)(&x), this, *(uint32_t*)(&this));
+		if (vflag) {
+			printf(FMT,
+			n, x, *(uint32_t*)(&x), this, *(uint32_t*)(&this));
+		}
 		x = (1.0) / ++n;
 		next = this + x;
 	} while (next > this);
